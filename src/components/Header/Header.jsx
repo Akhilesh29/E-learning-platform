9a4f396cd39c8d6964c0ec9eb2c8ed67072c42cd
Akhilesh <20203@iiitu.ac.in>
2022-12-27 23:54:36 +0530

    design half part of mentor page(7th)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -78,3 +78,86 @@ const Header= () => {
     )
 }
 export default Header;
+// import logo from './images/logo.svg';
+// import cart from './images/cart.svg';
+// import account from './images/account.svg';
+// import downArrow from './images/downarrow.svg';
+// import './nav.css';
+// import 'bootstrap/dist/css/bootstrap.css';
+// import {Container , Row, Col} from 'react-bootstrap'
+// import {Navbar, Nav, NavDropdown} from 'react-bootstrap'
+// import ThemeProvider from 'react-bootstrap/ThemeProvider'
+
+
+// function navbar(){
+//     return (
+//         <Navbar expand="lg" sticky="top" id='navs'>
+//             <Container fluid>
+//                     <Navbar.Brand  id="logo" href="#home"><img src={logo} alt="logo" /></Navbar.Brand>
+               
+
+//                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                     <Navbar.Collapse id="basic-navbar-nav">
+                
+//                     <Nav className="me-auto custom-nav">
+//                         <Nav.Link id="shop-link" href="#shop">Shop</Nav.Link>
+//                         {/* <Nav.Link href="#link">Link</Nav.Link> */}
+//                         <NavDropdown title="For Kinder Garden" id="basic-nav-dropdown">
+//                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.2">
+//                             Another action
+//                         </NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//                         <NavDropdown.Divider />
+//                         <NavDropdown.Item href="#action/3.4">
+//                             Separated link
+//                         </NavDropdown.Item>
+//                         </NavDropdown>
+
+//                         <NavDropdown title="For High School" id="basic-nav-dropdown">
+//                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.2">
+//                             Another action
+//                         </NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//                         <NavDropdown.Divider />
+//                         <NavDropdown.Item href="#action/3.4">
+//                             Separated link
+//                         </NavDropdown.Item>
+//                         </NavDropdown>
+//                         <NavDropdown title="For Colleges" id="basic-nav-dropdown">
+//                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.2">
+//                             Another action
+//                         </NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//                         <NavDropdown.Divider />
+//                         <NavDropdown.Item href="#action/3.4">
+//                             Separated link
+//                         </NavDropdown.Item>
+//                         </NavDropdown>
+                   
+//                         <NavDropdown title="Courses" id="basic-nav-dropdown">
+//                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.2">
+//                             Another action
+//                         </NavDropdown.Item>
+//                         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+//                         <NavDropdown.Divider />
+//                         <NavDropdown.Item href="#action/3.4">
+//                             Separated link
+//                         </NavDropdown.Item>
+//                         </NavDropdown>
+//                         </Nav>
+//                     <Nav>
+//                             <Nav.Link id="cart-link" href="#Cart">Cart(0) <img src={cart} alt="cart" /> </Nav.Link>
+//                             <Nav.Link id="account-link" href="#Account">My Account <img src={account} alt="Account" /> </Nav.Link>
+//                     </Nav>
+                  
+//                     </Navbar.Collapse>
+//           </Container>
+//         </Navbar>
+//       );
+// }
+
+// export default navbar;
--- src/pages/mentorpage/Mentor.jsx
+++ src/pages/mentorpage/Mentor.jsx
@@ -1,7 +1,60 @@
-import React from "react"
-const Mentor= () => {
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import './mentor.css';
+
+const Mentor = () => {
     return (
-        <h1> this is Mentor Page</h1>
-    )
+        <>
+            <Header />
+            <div className="Mentor">
+                <div className="Mentor-1">
+                    <div className="Mentor-1-1">
+                        <div className="Mentor-1-1-1">Home |</div>
+                        <div className="Mentor-1-1-2">Our Mentors</div>
+                    </div>
+                    <div className="Mentor-title">Eduvi has the<br />qualified mentor</div>
+                </div>
+
+                <div className="Mentor-2">
+                    <div className="Mentor-2-1"> All Mentors</div>
+                    <div className="Mentor-2-2"> For Kindergarten</div>
+                    <div className="Mentor-2-3"> For high school</div>
+                    <div className="Mentor-2-4"> For college</div>
+                    <div className="Mentor-2-5"> For Technology</div>
+                </div>
+                <div className="Mentor-3">
+                <div className="Mentor-3-1"> 
+                <div className="Mentor-3-1-1"></div>
+                <div className="Mentor-3-1-2">Kristin Watson</div>
+                <div className="Mentor-3-1-3">Founder & Mentor</div>
+                {/* <div className="Mentor-3-1-3">^</div> */}
+                </div>
+                <div className="Mentor-3-2"> 
+                <div className="Mentor-3-2-1"></div>
+                <div className="Mentor-3-2-2">Brooklyn Simmons</div>
+                <div className="Mentor-3-2-3">Founder & Mentor</div>
+                </div>
+                <div className="Mentor-3-3"> 
+                <div className="Mentor-3-3-1"></div>
+                <div className="Mentor-3-3-2">Robert Fox </div>
+                <div className="Mentor-3-3-3">Founder & Mentor</div>
+                </div>
+                <div className="Mentor-3-4"> 
+                <div className="Mentor-3-4-1"></div>
+                <div className="Mentor-3-4-2">Wade Warren </div>
+                <div className="Mentor-3-4-3">Founder & Mentor</div>
+                </div>
+                <div className="Mentor-3-5"> 
+                <div className="Mentor-3-5-1"></div>
+                <div className="Mentor-3-5-2">Bessie Cooper </div>
+                <div className="Mentor-3-5-3">Founder & Mentor</div>
+                </div>
+                </div>
+                </div>
+            <Footer />
+        </>
+
+    );
 }
 export default Mentor;
