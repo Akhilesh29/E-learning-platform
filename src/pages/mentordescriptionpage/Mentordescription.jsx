1bd36fc42e7e75e67318be56d583cddba4d97ca4
Akhilesh <20203@iiitu.ac.in>
2022-12-29 21:42:13 +0530

    design the half part of 8th page

--- src/pages/mentordescriptionpage/Mentordescription.jsx
+++ src/pages/mentordescriptionpage/Mentordescription.jsx
@@ -1,7 +1,34 @@
-import React from "react"
-const Mentordescription= () => {
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import './Mentordescription.css';
+
+const Mentor = () => {
     return (
-        <h1> this is Mentordescription Page</h1>
-    )
+        <>
+            <Header />
+
+            <div className="Mentordescription">
+                <div className="mentordescription-1">
+                    <div className="mentordescription-1-1">
+                        <div className="mentordescription-1-1-1">Home |</div>
+                        <div className="mentordescription-1-1-2">Mentor |</div>
+                        <div className="mentordescription-1-1-3">Kristin Watson</div>
+                        
+                        <div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="mentordescription-2">
+            <div className="mentordescription-2-1">Contact Now</div>
+
+            </div>
+
+            <Footer />
+        </>
+
+    );
 }
-export default Mentordescription;
+export default Mentor;
--- src/pages/mentorpage/Mentor.jsx
+++ src/pages/mentorpage/Mentor.jsx
@@ -24,34 +24,53 @@ const Mentor = () => {
                     <div className="Mentor-2-5"> For Technology</div>
                 </div>
                 <div className="Mentor-3">
-                <div className="Mentor-3-1"> 
-                <div className="Mentor-3-1-1"></div>
-                <div className="Mentor-3-1-2">Kristin Watson</div>
-                <div className="Mentor-3-1-3">Founder & Mentor</div>
-                {/* <div className="Mentor-3-1-3">^</div> */}
-                </div>
-                <div className="Mentor-3-2"> 
-                <div className="Mentor-3-2-1"></div>
-                <div className="Mentor-3-2-2">Brooklyn Simmons</div>
-                <div className="Mentor-3-2-3">Founder & Mentor</div>
-                </div>
-                <div className="Mentor-3-3"> 
-                <div className="Mentor-3-3-1"></div>
-                <div className="Mentor-3-3-2">Robert Fox </div>
-                <div className="Mentor-3-3-3">Founder & Mentor</div>
-                </div>
-                <div className="Mentor-3-4"> 
-                <div className="Mentor-3-4-1"></div>
-                <div className="Mentor-3-4-2">Wade Warren </div>
-                <div className="Mentor-3-4-3">Founder & Mentor</div>
-                </div>
-                <div className="Mentor-3-5"> 
-                <div className="Mentor-3-5-1"></div>
-                <div className="Mentor-3-5-2">Bessie Cooper </div>
-                <div className="Mentor-3-5-3">Founder & Mentor</div>
-                </div>
-                </div>
-                </div>
+                    <div className="Mentor-3-1">
+                        <div className="Mentor-3-1-1"></div>
+                        <div className="Mentor-3-1-2">Kristin Watson</div>
+                        <div className="Mentor-3-1-3">Founder & Mentor</div>
+                        {/* <div className="Mentor-3-1-3">^</div> */}
+                    </div>
+                    <div className="Mentor-3-2">
+                        <div className="Mentor-3-2-1"></div>
+                        <div className="Mentor-3-2-2">Brooklyn Simmons</div>
+                        <div className="Mentor-3-2-3">Founder & Mentor</div>
+                    </div>
+                    <div className="Mentor-3-3">
+                        <div className="Mentor-3-3-1"></div>
+                        <div className="Mentor-3-3-2">Robert Fox </div>
+                        <div className="Mentor-3-3-3">Founder & Mentor</div>
+                    </div>
+                    <div className="Mentor-3-4">
+                        <div className="Mentor-3-4-1"></div>
+                        <div className="Mentor-3-4-2">Wade Warren </div>
+                        <div className="Mentor-3-4-3">Founder & Mentor</div>
+                    </div>
+                    <div className="Mentor-3-5">
+                        <div className="Mentor-3-5-1"></div>
+                        <div className="Mentor-3-5-2">Bessie Cooper </div>
+                        <div className="Mentor-3-5-3">Founder & Mentor</div>
+                    </div>
+                    <div className="Mentor-3-6">
+                        <div className="Mentor-3-6-1"></div>
+                        <div className="Mentor-3-6-2">Ronald Richards </div>
+                        <div className="Mentor-3-6-3">Founder & Mentor</div>
+                    </div>
+                    <div className="Mentor-3-7-1"></div>
+                    <div className="Mentor-3-7-2">Guy Hawkins </div>
+                    <div className="Mentor-3-7-3">Founder & Mentor</div>
+                    <div className="Mentor-3-7-4"></div>
+                    <div className="Mentor-3-7-5"> 4.9 (135) </div>
+                    <div className="Mentor-3-8-1"></div>
+                    <div className="Mentor-3-8-2">Floyd Miles </div>
+                    <div className="Mentor-3-8-3">Founder & Mentor</div>
+                    <div className="Mentor-4">
+                    <div className="Mentor-4-1">
+                    <div className="Mentor-4-1-1"></div>
+                    <div className="Mentor-4-1-2"></div>
+            </div>
+            </div>
+            </div>
+            </div>
             <Footer />
         </>
 
